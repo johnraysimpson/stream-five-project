e89c52b60453d7d4ed59771c8078470588f96a4e
Ubuntu <ubuntu@ip-172-31-39-122.ec2.internal>
2019-11-19 17:09:05 +0000

    styled search pages, edited search views to work properly

--- search/views.py
+++ search/views.py
@@ -7,24 +7,37 @@ from profiles.models import ParentProfile, Student, TutorProfile
 # Create your views here.
 def add_or_search_parent_view(request):
     """A view that renders page for adding a new parent user or searching for an existing one"""
-    parents = ParentProfile.objects.annotate(full_name=Concat('first_name', V(' '), 'last_name')).filter(Q(first_name__icontains=request.GET.get('q', ''))|
-                                                                                                            Q(last_name__icontains=request.GET.get('q', ''))|
-                                                                                                            Q(full_name__icontains=request.GET.get('q', ''))|
-                                                                                                            Q(user__centre=request.user.centre)).order_by('last_name')
+    try:
+        parents = ParentProfile.objects.annotate(full_name=Concat('first_name', V(' '), 'last_name')).filter(   Q(first_name__icontains=request.GET.get('q'))|
+                                                                                                                Q(last_name__icontains=request.GET.get('q'))|
+                                                                                                                Q(full_name__icontains=request.GET.get('q')),
+                                                                                                                user__centre=request.user.centre
+                                                                                                                ).order_by('last_name')
+    except:
+        parents = ParentProfile.objects.filter(user__centre=request.user.centre).order_by('last_name')
+    print(request.GET.get('q'))
     return render(request, 'parents.html', {'parents': parents})
     
 def search_student_view(request):
     """A view that renders page for searching for a student"""
-    students = Student.objects.annotate(full_name=Concat('first_name', V(' '), 'last_name')).filter(Q(first_name__icontains=request.GET.get('q', ''))|
-                                                                                                    Q(last_name__icontains=request.GET.get('q', ''))|
-                                                                                                    Q(full_name__icontains=request.GET.get('q', ''))|
-                                                                                                    Q(parent__user__centre=request.user.centre)).order_by('last_name')
+    try:
+        students = Student.objects.annotate(full_name=Concat('first_name', V(' '), 'last_name')).filter(Q(first_name__icontains=request.GET.get('q', ''))|
+                                                                                                        Q(last_name__icontains=request.GET.get('q', ''))|
+                                                                                                        Q(full_name__icontains=request.GET.get('q', '')),
+                                                                                                        parent__user__centre=request.user.centre
+                                                                                                        ).order_by('last_name')
+    except:
+        students = Student.objects.filter(parent__user__centre=request.user.centre).order_by('last_name')
     return render(request, 'students.html', {'students': students})
     
 def add_or_search_tutor_view(request):
     """A view that renders page for adding a new tutor user or searching for an existing one"""
-    tutors = TutorProfile.objects.annotate(full_name=Concat('first_name', V(' '), 'last_name')).filter(Q(first_name__icontains=request.GET.get('q', ''))|
-                                                                                                    Q(last_name__icontains=request.GET.get('q', ''))|
-                                                                                                    Q(full_name__icontains=request.GET.get('q', ''))|
-                                                                                                    Q(user__centre=request.user.centre)).order_by('last_name')
+    try:
+        tutors = TutorProfile.objects.annotate(full_name=Concat('first_name', V(' '), 'last_name')).filter( Q(first_name__icontains=request.GET.get('q', ''))|
+                                                                                                            Q(last_name__icontains=request.GET.get('q', ''))|
+                                                                                                            Q(full_name__icontains=request.GET.get('q', '')),
+                                                                                                            user__centre=request.user.centre
+                                                                                                            ).order_by('last_name')
+    except:
+        tutors = TutorProfile.objects.filter(user__centre=request.user.centre).order_by('last_name')
     return render(request, 'tutors.html', {'tutors': tutors})
